ca2b3c7c1a78d75dbaee8f76d4e13ce41ceea884
Miriam Trapero Rodríguez <mimi23m2002@gmail.com>
2021-09-20 18:35:24 +0200

    Ejercicio 3a

---
+++ PRIMERAPRACTICA/Assets/Scripts/MiniPractica.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniPractica : MonoBehaviour
+{
+    //Ejercicio 3a
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Saludar("Miriam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+
+    void Saludar(string nombre)
+    {
+        print("Hola "+ nombre);
+    }
+   
+   
+
+
+
+
+}
