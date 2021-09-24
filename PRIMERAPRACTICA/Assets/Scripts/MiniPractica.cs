34a9f87bf6a3b50a9677bc3d48348a350de3c3f3
Miriam Trapero Rodríguez <>
2021-09-24 16:10:31 +0200

    Ejercicio 3c

--- PRIMERAPRACTICA/Assets/Scripts/MiniPractica.cs
+++ PRIMERAPRACTICA/Assets/Scripts/MiniPractica.cs
@@ -1,56 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniPractica : MonoBehaviour
 {
-    //Ejercicio 3b
+    //Ejercicio 3c
 
-    [SerializeField] int n1;
-    [SerializeField] int n2;
-
-    bool devolver;   //Variable booleana para devolver el resultado
-    int Resultadofinal; 
+    [SerializeField] Text MyText;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        Matematica();
-
+        StartCoroutine("Cronometro");
     }
 
     // Update is called once per frame
     void Update()
     {
-      
 
     }
 
-    void Matematica()
+    IEnumerator Cronometro()
     {
-
-        Resultadofinal = n1 + n2;  
-        if (Resultadofinal<=5)
+        for (int n = 0; n <= 30; n++)
         {
-
-            devolver = true;
-
+            MyText.text = "Contador: " + n;
+            yield return new WaitForSeconds(1f);
+            if (n == 30)
+            {
+                StopCoroutine("Cronometro");
+            }
         }
-        else
-        {
-            devolver = false;
-        }
-
-        print("El resultado es: " + devolver);
-       
-        return;
-
     }
+}
 
 
-
-
-
-
-    }
